9430241563c177434b043aab21118bfd4e614c1f
Marco La Rosa <m@lr.id.au>
2019-09-05 11:04:29 +1000

    implement header and footer skeletons

---
+++ ui/src/components/Header.component.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <router-link to="/">
+            <i class="fas fa-home"></i> Home
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- ui/src/components/app.vue
+++ ui/src/components/app.vue
@@ -1,17 +1,39 @@
 <template>
-    <div>
-        <router-view></router-view>
+    <div class="flex flex-col h-full">
+        <header class="w-full pin-t p-4 style-header">
+            <header-component></header-component>
+        </header>
+        <div id="content" class="flex-1 px-4 py-2">
+            <router-view></router-view>
+        </div>
+        <footer class="w-full pin-b style-footer p-4">
+            <div class="text-xs">
+                <router-link to="/health-check">
+                    <i class="fas fa-heartbeat"></i> Health Check
+                </router-link>
+            </div>
+        </footer>
     </div>
 </template>
 
 <script>
+import HeaderComponent from "./Header.component.vue";
 export default {
     el: "#app",
+    components: {
+        HeaderComponent
+    },
     async mounted() {
         this.$store.dispatch("initialise", { $router: this.$router });
     }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+// .style-header {
+//     background-color: red;
+// }
+// .style-footer {
+//     background-color: yellow;
+// }
 </style>
