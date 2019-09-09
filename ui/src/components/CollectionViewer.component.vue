aee3faa353d635881aec97e52aee92bcb5eca070
Marco La Rosa <m@lr.id.au>
2019-09-09 17:13:03 +1000

    scaffold out code to load data as route params change

--- ui/src/components/CollectionViewer.component.vue
+++ ui/src/components/CollectionViewer.component.vue
@@ -1,13 +1,58 @@
 <template>
-    <div>collection: {{$route.params.domain}}/{{$route.params.collectionId}}</div>
+    <div class="flex flex-col">
+        <div>collection: {{$route.params.domain}}/{{$route.params.collectionId}}</div>
+        <div>
+            <pre>{{collection}}</pre>
+        </div>
+    </div>
 </template>
 
 <script>
+import { DataLoader } from "src/data-loader.service";
+const dataLoader = new DataLoader();
+
 export default {
     data() {
-        return {};
+        return {
+            watchers: {},
+            collection: {}
+        };
     },
-    mounted() {}
+    mounted() {
+        const domain = this.$store.state.configuration.domain;
+        if (domain) {
+            this.watchers.domain = this.$watch(
+                "$route.params.domain",
+                this.loadCollection
+            );
+            this.watchers.collectionId = this.$watch(
+                "$route.params.collectionId",
+                this.loadCollection
+            );
+        } else {
+            this.watchers.collectionId = this.$watch(
+                "$route.params.collectionId",
+                this.loadCollection
+            );
+        }
+        this.loadCollection();
+    },
+    beforeDestroy() {
+        if (this.watchers.domain) this.watchers.domain();
+        this.watchers.collectionId();
+    },
+    methods: {
+        async loadCollection() {
+            const domain =
+                this.$route.params.domain ||
+                this.$store.state.configuration.domain;
+            const collectionId = this.$route.params.collectionId;
+            this.collection = await dataLoader.loadCollection({
+                domain,
+                collectionId
+            });
+        }
+    }
 };
 </script>
 
--- ui/src/components/ItemViewer.component.vue
+++ ui/src/components/ItemViewer.component.vue
@@ -5,9 +5,52 @@
 <script>
 export default {
     data() {
-        return {};
+        return {
+            watchers: {}
+        };
     },
-    mounted() {}
+    mounted() {
+        const domain = this.$store.state.configuration.domain;
+        if (domain) {
+            this.watchers.domain = this.$watch(
+                "$route.params.domain",
+                this.loadItem
+            );
+            this.watchers.collectionId = this.$watch(
+                "$route.params.collectionId",
+                this.loadItem
+            );
+            this.watchers.itemId = this.$watch(
+                "$route.params.itemId",
+                this.loadItem
+            );
+        } else {
+            this.watchers.collectionId = this.$watch(
+                "$route.params.collectionId",
+                this.loadItem
+            );
+            this.watchers.itemId = this.$watch(
+                "$route.params.itemId",
+                this.loadItem
+            );
+        }
+        this.loadItem();
+    },
+    beforeDestroy() {
+        if (this.watchers.domain) this.watchers.domain();
+        this.watchers.collectionId();
+        this.watchers.itemId();
+    },
+    methods: {
+        loadItem() {
+            const domain =
+                this.$route.params.domain ||
+                this.$store.state.configuration.domain;
+            const collectionId = this.$route.params.collectionId;
+            const itemId = this.$route.params.itemId;
+            console.log(domain, collectionId, itemId);
+        }
+    }
 };
 </script>
 
