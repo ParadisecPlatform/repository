ce764f6ceae7c9f7005f42799aa31af5cfa324aa
Marco La Rosa <m@lr.id.au>
2019-09-09 15:30:24 +1000

    scaffold out collection and item viewer components

---
+++ ui/src/components/CollectionViewer.component.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>collection: {{$route.params.domain}}/{{$route.params.collectionId}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ ui/src/components/ItemViewer.component.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>item: {{$route.params.domain}}/{{$route.params.collectionId}}/{{$route.params.itemId}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
