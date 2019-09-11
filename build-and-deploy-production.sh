992957f1ae03939f8871f563f54c4fbbb5da1463
Marco La Rosa <m@lr.id.au>
2019-09-11 11:50:04 +1000

    scripts and code to run up a prod stack

---
+++ build-and-deploy-production.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ui
+npm run build:production
+cd -
+
+docker-compose -f docker-compose-production.yml up -d
+
+cd ocfl-indexer
+node indexer.js \
+    --source ../ocfl-repository \
+    --search http://localhost/search \
+    --username indexer \
+    --password somerandompassword
