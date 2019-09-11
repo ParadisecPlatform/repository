7e892e76fdda0fbee5416bb812a59de318b41895
Marco La Rosa <m@lr.id.au>
2019-09-11 12:01:43 +1000

    sleep before trying to index content

--- build-and-deploy-production.sh
+++ build-and-deploy-production.sh
@@ -6,6 +6,8 @@ cd -
 
 docker-compose -f docker-compose-production.yml up -d
 
+echo "Sleeping for 30 seconds so elastic has time to start"
+sleep 30
 cd ocfl-indexer
 node indexer.js \
     --source ../ocfl-repository \
