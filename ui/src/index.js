356e7c072671fa010ac0dd13d9d0ebfb97aa029a
Marco La Rosa <m@lr.id.au>
2019-10-17 11:44:30 +1100

    add google analytics

--- ui/src/index.js
+++ ui/src/index.js
@@ -9,11 +9,18 @@ import ElementUI from "element-ui";
 import locale from "element-ui/lib/locale/lang/en";
 import VueScrollTo from "vue-scrollto";
 import infiniteScroll from "vue-infinite-scroll";
+import VueAnalytics from "vue-analytics";
 import { VueMasonryPlugin } from "vue-masonry";
 Vue.use(ElementUI, { locale });
 Vue.use(VueScrollTo);
 Vue.use(infiniteScroll);
 Vue.use(VueMasonryPlugin);
+Vue.use(VueAnalytics, {
+    id: "UA-79571514-3",
+    debug: {
+        sendHitTask: process.env.NODE_ENV === "production"
+    }
+});
 
 import App from "components/app.vue";
 import { router } from "./routes";
