adb0276cd465d48b59ffe2cc0744fbb0b5b0edd7
Marco La Rosa <m@lr.id.au>
2020-01-20 10:16:29 +1100

    implement method to cast boolean to string true || false

--- ui/src/filters.js
+++ ui/src/filters.js
@@ -9,3 +9,19 @@ export function date(value, fmt) {
     }
     return format(value, "dd/MM/yyyy");
 }
+
+export function toBoolean(string) {
+    switch (string.toLowerCase().trim()) {
+        case "true":
+        case "yes":
+        case "1":
+            return "True";
+        case "false":
+        case "no":
+        case "0":
+        case null:
+            return "False";
+        default:
+            return String(Boolean(string));
+    }
+}
