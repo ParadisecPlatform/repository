7a939dcab1ce49930624a989cf304df1374bed4f
Marco La Rosa <m@lr.id.au>
2020-01-21 08:48:43 +1100

    revise script - tools now separate to this repo

--- build-and-deploy-production.sh
+++ build-and-deploy-production.sh
@@ -6,11 +6,11 @@ cd -
 
 \sudo docker-compose -f docker-compose-production.yml up -d
 
-echo "Sleeping for 30 seconds so elastic has time to start"
-sleep 30
-cd ocfl-indexer
-node indexer.js \
-    --source ../ocfl-repository \
-    --search http://localhost/search \
-    --username indexer \
-    --password somerandompassword
+#echo "Sleeping for 30 seconds so elastic has time to start"
+#sleep 30
+#cd ocfl-indexer
+#node indexer.js \
+#    --source ../ocfl-repository \
+#    --search http://localhost/search \
+#    --username indexer \
+#    --password somerandompassword
