eddbb2796698dd75ef7c878dfac20064c189cca0
Marco La Rosa <m@lr.id.au>
2020-02-04 11:55:04 +1100

    show a warning if no apparent license exists

--- ui/src/components/view/paradisec.org.au/License.component.vue
+++ ui/src/components/view/paradisec.org.au/License.component.vue
@@ -1,17 +1,24 @@
 <template>
     <div class="my-2">
-        <el-alert :type="type" :closable="false" effect="dark" :center="true">
-            <div class="text-lg text-center">
-                {{ data.objectifiedCrate.license.name }}
-            </div>
-            <div v-if="type === 'error'" class="text-sm">
-                {{ data.objectifiedCrate.license.description }}
-            </div>
-        </el-alert>
+        <div v-if="licenseDefined">
+            <el-alert :type="type" :closable="false" effect="dark" :center="true">
+                <div class="text-lg text-center">{{ data.objectifiedCrate.license.name }}</div>
+                <div
+                    v-if="type === 'error'"
+                    class="text-sm"
+                >{{ data.objectifiedCrate.license.description }}</div>
+            </el-alert>
+        </div>
+        <div v-else>
+            <el-alert type="warning" :closable="false" effect="dark" :center="true">
+                <span class="text-lg">No license seems to be defined for this item</span>
+            </el-alert>
+        </div>
     </div>
 </template>
 
 <script>
+import { isEmpty } from "lodash";
 export default {
     props: {
         data: {
@@ -21,16 +28,29 @@ export default {
     },
     data() {
         return {
-            type: undefined
+            licenseDefined:
+                isEmpty(this.data.objectifiedCrate.license.name) &&
+                isEmpty(this.data.objectifiedCrate.license.description)
+                    ? false
+                    : true,
+            showLicenseName: isEmpty(this.data.objectifiedCrate.license.name)
+                ? false
+                : true,
+            showLicenseDescription: isEmpty(this.data.objectifiedCrate.license)
+                .description
+                ? false
+                : true
         };
     },
-    mounted() {
-        try {
-            this.type = this.data.objectifiedCrate.license.name.match(/^Open/)
-                ? "success"
-                : "error";
-        } catch (error) {
-            this.type = "error";
+    computed: {
+        type: function() {
+            try {
+                return this.data.objectifiedCrate.license.name.match(/^Open/)
+                    ? "success"
+                    : "error";
+            } catch (error) {
+                return "error";
+            }
         }
     }
 };
