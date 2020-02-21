cf7404e52de926dd57bc6e1711b8a804a0313a59
Marco La Rosa <m@lr.id.au>
2020-02-21 16:37:06 +1100

    rework to use transcription parsers; remove legacy code

--- ui/src/services/data-loader.service.js
+++ ui/src/services/data-loader.service.js
@@ -5,18 +5,8 @@ import shajs from "sha.js";
 import jsonld from "jsonld";
 import { flattenDeep, isPlainObject, orderBy, groupBy } from "lodash";
 
-import { EAFParser, SimpleEAFParser } from "./eaf-parser.service";
-import { IXTParser } from "./ixt-parser.service";
-import { TRSParser } from "./trs-parser.service";
-import { FlextextParser } from "./flextext-parser.service";
+import { Parser } from "transcription-parsers";
 import ROCrate from "ro-crate/lib/rocrate";
-
-const parser = {
-    eaf: new EAFParser(),
-    ixt: new IXTParser(),
-    trs: new TRSParser(),
-    flextext: new FlextextParser()
-};
 
 export class DataLoader {
     constructor() {
@@ -246,114 +236,12 @@ export class DataLoader {
         const type = transcription.name.split(".").pop();
         let response = await fetch(transcription.path);
         if (!response.ok) throw response;
-        let xml = await response.text();
-        let segments, tiers;
-        if (type === "eaf") {
-            let result = await parser.eaf.extract({ data: xml });
-            segments = result.segments;
-            tiers = result.tiers;
-        } else {
-            xml = this.parseXML(xml);
-            if (!xml) return [];
-            transcription = this.convertXmlToJson(xml);
-            segments = parser[type].parse(transcription);
-            segments = segments.map(segment => {
-                return {
-                    ...segment,
-                    htmlId: `s${this.hash(segment.id)}`
-                };
-            });
-        }
-        return { type, segments, tiers };
-    }
-
-    parseXML(src) {
-        /* returns an XMLDocument, or null if `src` is malformed */
-
-        let key = `a` + Math.random().toString(32);
-
-        let parser = new DOMParser();
-
-        let doc = null;
-        try {
-            doc = parser.parseFromString(src + `<?${key}?>`, `application/xml`);
-        } catch (_) {}
-
-        if (!(doc instanceof XMLDocument)) {
-            return null;
-        }
-
-        let lastNode = doc.lastChild;
-        if (
-            !(lastNode instanceof ProcessingInstruction) ||
-            lastNode.target !== key ||
-            lastNode.data !== ``
-        ) {
-            return null;
-        }
-
-        doc.removeChild(lastNode);
-
-        let errElemCount = doc.documentElement.getElementsByTagName(
-            `parsererror`
-        ).length;
-        if (errElemCount !== 0) {
-            let errDoc = null;
-            try {
-                errDoc = parser.parseFromString(src + `<?`, `application/xml`);
-            } catch (_) {}
-
-            if (
-                !(errDoc instanceof XMLDocument) ||
-                errDoc.documentElement.getElementsByTagName(`parsererror`)
-                    .length === errElemCount
-            ) {
-                return null;
-            }
-        }
-
-        return doc;
-    }
-
-    convertXmlToJson(xml) {
-        // http://davidwalsh.name/convert-xml-json
-        // Changes XML to JSON
-        // Create the return object
-        var obj = {};
-
-        if (xml.nodeType === 1) {
-            // element
-            // do attributes
-            if (xml.attributes.length > 0) {
-                obj["@attributes"] = {};
-                for (var j = 0; j < xml.attributes.length; j++) {
-                    var attribute = xml.attributes.item(j);
-                    obj["@attributes"][attribute.nodeName] =
-                        attribute.nodeValue;
-                }
-            }
-        } else if (xml.nodeType === 3) {
-            // text
-            obj = xml.nodeValue;
-        }
-
-        // do children
-        if (xml.hasChildNodes()) {
-            for (var i = 0; i < xml.childNodes.length; i++) {
-                var item = xml.childNodes.item(i);
-                var nodeName = item.nodeName;
-                if (typeof obj[nodeName] === "undefined") {
-                    obj[nodeName] = this.convertXmlToJson(item);
-                } else {
-                    if (typeof obj[nodeName].push === "undefined") {
-                        var old = obj[nodeName];
-                        obj[nodeName] = [];
-                        obj[nodeName].push(old);
-                    }
-                    obj[nodeName].push(this.convertXmlToJson(item));
-                }
-            }
-        }
-        return obj;
+        let xmlString = await response.text();
+        let parser = new Parser({
+            name: transcription.name,
+            data: xmlString
+        });
+        let result = await parser.parse();
+        return result;
     }
 }
