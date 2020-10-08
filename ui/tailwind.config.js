c18058587c0c7c94c66f68cf669673b92bcd239b
Marco La Rosa <m@lr.id.au>
2020-10-08 16:05:58 +1100

    add primary and highlight colour definitions

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
     theme: {
+        extend: {
+            colors: {
+                primary: "#1f3a93",
+                highlight: "#f9690e",
+            },
+        },
         screens: {
             sm: "640px",
             md: "768px",
             lg: "1024px",
             xl: "1280px",
-            xxl: "1600px"
-        }
-    }
+            xxl: "1600px",
+        },
+    },
 };
