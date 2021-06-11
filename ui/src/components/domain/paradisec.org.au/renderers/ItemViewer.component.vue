91bb59289aa1dbe13d5eb2538929489fa1b08512
Marco La Rosa <m@lr.id.au>
2021-06-12 09:49:04 +1000

    restyle as tabbed layout for metadata and content

--- ui/src/components/domain/paradisec.org.au/renderers/ItemViewer.component.vue
+++ ui/src/components/domain/paradisec.org.au/renderers/ItemViewer.component.vue
@@ -1,76 +1,82 @@
 <template>
     <div>
         <div class="flex flex-col" id="content" v-if="item && item.name">
-            <div class="flex flex-col space-y-2">
-                <div class="my-4 text-3xl">{{ item.name }}</div>
-                <div class="mb-2">
-                    <license-component :data="item.license[0]" />
-                </div>
-                <div class="flex flex-row space-x-2">
-                    <div class="flex flex-col space-y-2 flex-grow">
-                        <div class="text-sm">
-                            Item Identifier {{ item.collectionIdentifier }} /
-                            {{ item.itemIdentifier }}
+            <el-tabs type="border-card" tab-position="top" v-model="activeTab">
+                <el-tab-pane label="Metadata" name="metadata">
+                    <div class="flex flex-col space-y-2">
+                        <div class="my-4 text-3xl">{{ item.name }}</div>
+                        <div class="mb-2">
+                            <license-component :data="item.license[0]" />
                         </div>
-                        <div class="text-sm">
-                            Collection
-                            <router-link :to="collectionLink(item.collectionIdentifier)">
-                                {{ item.collectionIdentifier }}
-                            </router-link>
+                        <div class="flex flex-row space-x-2">
+                            <div class="flex flex-col space-y-2 flex-grow">
+                                <div class="text-sm">
+                                    Item Identifier {{ item.collectionIdentifier }} /
+                                    {{ item.itemIdentifier }}
+                                </div>
+                                <div class="text-sm">
+                                    Collection
+                                    <router-link :to="collectionLink(item.collectionIdentifier)">
+                                        {{ item.collectionIdentifier }}
+                                    </router-link>
+                                </div>
+
+                                <div class="p-6 rounded">
+                                    {{ item.description }}
+                                </div>
+
+                                <render-set-component
+                                    name="Contributors"
+                                    :items="item.contributor"
+                                    :fields="['displayName']"
+                                    layout="{displayName}"
+                                />
+
+                                <render-set-component
+                                    name="Publisher"
+                                    :items="item.publisher"
+                                    :fields="['name']"
+                                    layout="{name}"
+                                />
+
+                                <render-set-component name="Countries" :items="item.countries" />
+
+                                <render-set-component
+                                    name="Fields of Research"
+                                    :items="item.fieldsOfResearch"
+                                    :fields="['name', 'identifier']"
+                                    layout="{name} ({identifier})"
+                                />
+                            </div>
+                            <div>
+                                <render-location-component
+                                    :content-location="item.contentLocation"
+                                    width="200"
+                                    height="200"
+                                    class="p-4 border border-gray-600"
+                                />
+                            </div>
                         </div>
 
-                        <div class="p-6 rounded">
-                            {{ item.description }}
-                        </div>
+                        <render-item-information-component :item="item" />
 
                         <render-set-component
-                            name="Contributors"
-                            :items="item.contributor"
-                            :fields="['displayName']"
-                            layout="{displayName}"
+                            name="Subject Languages"
+                            :items="item.subjectLanguages"
+                            item-style="bg-yellow-400"
                         />
 
                         <render-set-component
-                            name="Publisher"
-                            :items="item.publisher"
-                            :fields="['name']"
-                            layout="{name}"
-                        />
-
-                        <render-set-component name="Countries" :items="item.countries" />
-
-                        <render-set-component
-                            name="Fields of Research"
-                            :items="item.fieldsOfResearch"
-                            :fields="['name', 'identifier']"
-                            layout="{name} ({identifier})"
+                            name="Content Languages"
+                            :items="item.contentLanguages"
+                            item-style="bg-yellow-400"
                         />
                     </div>
-                    <div>
-                        <render-location-component
-                            :content-location="item.contentLocation"
-                            width="200"
-                            height="200"
-                            class="p-4 border border-gray-600"
-                        />
-                    </div>
-                </div>
-
-                <render-item-information-component :item="item" />
-
-                <render-set-component
-                    name="Subject Languages"
-                    :items="item.subjectLanguages"
-                    item-style="bg-yellow-400"
-                />
-
-                <render-set-component
-                    name="Content Languages"
-                    :items="item.contentLanguages"
-                    item-style="bg-yellow-400"
-                />
-                <render-content-component :data="data" />
-            </div>
+                </el-tab-pane>
+                <el-tab-pane label="Content" name="content">
+                    <render-content-component :data="data" />
+                </el-tab-pane>
+            </el-tabs>
         </div>
     </div>
 </template>
@@ -101,6 +107,7 @@ export default {
     data() {
         return {
             item: {},
+            activeTab: "metadata",
         };
     },
     mounted() {
