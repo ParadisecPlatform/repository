4b81dd7d12ac0d3c035418cb80fad05e057c42c7
Marco La Rosa <m@lr.id.au>
2021-06-17 16:16:37 +1000

    add route handlers to map from nabu url to new

---
+++ ui/src/components/domain/paradisec.org.au/legacy/LegacyRouter.component.vue
@@ -0,0 +1,71 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { DataLoader } from "src/services/data-loader.service";
+import { getFilesByName, getFilesByEncoding } from "components/shared/media-player/lib";
+const dataLoader = new DataLoader();
+
+export default {
+    data() {
+        return {};
+    },
+    mounted() {
+        this.init();
+    },
+    methods: {
+        async init() {
+            const collectionId = this.$route.params.collectionId;
+            const itemId = this.$route.params.itemId;
+            const essenceId = this.$route.params.essenceId;
+            if (collectionId && !itemId && !essenceId) {
+                this.$router.push({ path: `/view/${collectionId}` });
+            } else if (collectionId && itemId && !essenceId) {
+                this.$router.push({ path: `/view/${collectionId}/${itemId}` });
+            } else if (collectionId && itemId && essenceId) {
+                let params = {
+                    identifier: `/paradisec.org.au/${collectionId}/${itemId}`,
+                    version: undefined,
+                    configuration: this.$store.state.configuration,
+                };
+
+                this.ocflObject = await dataLoader.load({ ...params });
+                let essence = this.ocflObject.rocrate["@graph"].filter(
+                    (i) => i.essenceId == essenceId
+                )[0];
+
+                const configuration = this.$store.state.configuration;
+                let type, hash;
+                if (configuration.imageFormats.includes(essence.encodingFormat)) {
+                    type = "image";
+                    hash = essence.name;
+                } else if (configuration.audioFormats.includes(essence.encodingFormat)) {
+                    type = "audio";
+                    hash = essence.name.split(".").shift();
+                } else if (configuration.videoFormats.includes(essence.encodingFormat)) {
+                    type = "video";
+                    hash = essence.name.split(".").shift();
+                } else if (
+                    configuration.documentFileExtensions.includes(essence.name.split(".").pop())
+                ) {
+                    type = "document";
+                    hash = essence.name;
+                } else if (
+                    configuration.transcriptionFileExtensions.includes(
+                        essence.name.split(".").pop()
+                    )
+                ) {
+                    type = "xml";
+                    hash = essence.name;
+                }
+                this.$router.push({
+                    path: `/view/${collectionId}/${itemId}`,
+                    query: { type },
+                    hash: `#${hash}`,
+                });
+            }
+        },
+    },
+};
+</script>
--- ui/src/routes/paradisec.org.au.js
+++ ui/src/routes/paradisec.org.au.js
@@ -2,16 +2,30 @@
 
 import AdvancedSearchComponent from "components/domain/paradisec.org.au/advancedSearch/Shell.component.vue";
 import TranscriptionSearchComponent from "components/domain/paradisec.org.au/transcriptionSearch/Shell.component.vue";
+import ParadisecLegacyRouterComponent from "components/domain/paradisec.org.au/legacy/LegacyRouter.component.vue";
+
 export function router({ routes }) {
-    routes = routes.map(route => {
+    routes = routes.map((route) => {
         if (route.path === "/") {
             route.children.push({
                 path: "advanced-search",
-                component: AdvancedSearchComponent
+                component: AdvancedSearchComponent,
             });
             route.children.push({
                 path: "transcription-search",
-                component: TranscriptionSearchComponent
+                component: TranscriptionSearchComponent,
+            });
+            route.children.push({
+                path: "collections/:collectionId",
+                component: ParadisecLegacyRouterComponent,
+            });
+            route.children.push({
+                path: "collections/:collectionId/items/:itemId",
+                component: ParadisecLegacyRouterComponent,
+            });
+            route.children.push({
+                path: "collections/:collectionId/items/:itemId/essences/:essenceId",
+                component: ParadisecLegacyRouterComponent,
             });
         }
         return route;
