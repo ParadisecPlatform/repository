097996758c6d1baea2aebc4c3be475f800b45227
Marco La Rosa <m@lr.id.au>
2022-11-16 13:51:42 +1100

    refactor

--- api/src/routes/collection.js
+++ api/src/routes/collection.js
@@ -4,23 +4,34 @@ import { loadConfiguration } from "../common/index.js";
 import { Store } from "@coedl/nocfl-js";
 
 export function setupRoutes(fastify, options, done) {
+    fastify.addHook("preHandler", verifyCollectionExists);
+
     fastify.get("/collections/:collectionId/metadata", getCollectionMetadataHandler);
     done();
 }
 
-async function getCollectionMetadataHandler(req, res) {
+async function verifyCollectionExists(req, res) {
     let configuration = await loadConfiguration();
+
     let store = new Store({
         domain: configuration.domain,
         className: "collection",
         id: req.params.collectionId,
         credentials: configuration.api.s3,
     });
-
     const exists = await store.itemExists();
     if (!exists) {
         return res.notFound();
     }
-    let crate = await store.getJSON({ target: "ro-crate-metadata.json" });
+    req.session.configuration = configuration;
+    req.session.store = store;
+}
+
+async function getCollectionMetadataHandler(req, res) {
+    const exists = await req.session.store.itemExists();
+    if (!exists) {
+        return res.notFound();
+    }
+    let crate = await req.session.store.getJSON({ target: "ro-crate-metadata.json" });
     return { crate };
 }
