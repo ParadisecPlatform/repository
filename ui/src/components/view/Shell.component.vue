6fd6faccd2dcb79edab194013c0b8bac3e26b3ed
Marco La Rosa <m@lr.id.au>
2022-11-16 15:29:09 +1100

    try to load profile for the thing being rendered

--- ui/src/components/view/Shell.component.vue
+++ ui/src/components/view/Shell.component.vue
@@ -9,7 +9,7 @@
             <el-tab-pane label="Metadata" name="metadata">
                 <div v-if="data.activeTab === 'metadata'" v-loading="data.loading">
                     <describo-crate-builder
-                        v-if="!data.error && !data.loading"
+                        v-if="!data.error && !data.loading && data.expertMode"
                         :crate="data.crate"
                         :profile="data.profile"
                         :readonly="true"
@@ -33,34 +33,21 @@
 <script setup>
 import RenderContentComponent from "./RenderContent.component.vue";
 import { useRoute, useRouter } from "vue-router";
-import { inject, onMounted, onBeforeMount, reactive, nextTick } from "vue";
+import { getPath } from "../modules/content-viewer/lib.js";
+import { inject, onMounted, onBeforeMount, reactive } from "vue";
+import { CrateManager } from "../../crate-manager.js";
 const $route = useRoute();
 const $router = useRouter();
 const $http = inject("$http");
 
 let data = reactive({
     activeTab: "metadata",
+    expertMode: true,
     loading: false,
     error: false,
     type: undefined,
     crate: {},
-    profile: {
-        layouts: {
-            Dataset: [
-                {
-                    name: "About",
-                    description: "Key information about this item",
-                    inputs: ["identifier", "description", "contentLanguage", "subjectLanguage"],
-                },
-                {
-                    name: "Files",
-                    description: "Files forming part of this item",
-                    inputs: ["hasPart"],
-                },
-            ],
-        },
-    },
-    // profile: {},
+    profile: {},
 });
 
 onBeforeMount(() => {
@@ -72,25 +59,30 @@ onMounted(() => {
     init();
 });
 async function init() {
-    let response;
-
     data.loading = true;
     data.error = false;
     const { collectionId, itemId } = $route.params;
     if (collectionId && !itemId) {
-        response = await $http.get({ route: `/collections/${collectionId}/metadata` });
         data.type = "collection";
     } else if (collectionId && itemId) {
-        response = await $http.get({
-            route: `/collections/${collectionId}/items/${itemId}/metadata`,
-        });
         data.type = "item";
     } else if (itemId && !collectionId) {
-        response = await $http.get({ route: `/items/${itemId}/metadata` });
         data.type = "item";
     }
+
+    let response = await $http.get({ route: `${getPath($route.params)}/metadata` });
     if (response.status === 200) {
-        let { crate } = await response.json();
+        const { crate } = await response.json();
+        let c = new CrateManager({ crate });
+        const rootDatasetType = c.getRootDataset()["@type"].join(", ");
+
+        response = await $http.get({ route: `/profiles/${rootDatasetType}` });
+        if (response.status === 200) {
+            const { profile } = await response.json();
+            if (profile) {
+                data.profile = { ...profile };
+            }
+        }
         data.crate = { ...crate };
         data.loading = false;
     } else {
